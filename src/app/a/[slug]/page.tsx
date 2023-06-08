f7ef620a39fa93c3dc4f8cce70c8f89e3fdbbea3
Omar Sahyoun <omar@sumofus.org>
2023-06-08 13:40:43 +0100

    adds revalidate

--- src/app/a/[slug]/page.tsx
+++ src/app/a/[slug]/page.tsx
@@ -2,7 +2,7 @@ import BodyContent from "../../../components/BodyContent";
 
 async function getData(slug: string) {
   const url = `https://prosecco-staging.eko.org/api/pages/${slug}.json`;
-  const res = await fetch(url);
+  const res = await fetch(url, { next: { revalidate: 60 } });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
